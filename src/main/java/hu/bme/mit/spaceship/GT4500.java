68fb52c874492042be8bff734cb234db7c08110b
such8 <mate.hagyak@gmail.com>
2020-04-25 20:12:06 +0200

    added comment to test merge conflict

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -15,6 +15,9 @@ public class GT4500 implements SpaceShip {
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
 
+  /**
+   * just adding some comments to test merge confict
+   */
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
     return false;
